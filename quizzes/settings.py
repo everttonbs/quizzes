e2ee37c8aaa3d4506a7a940bc7c2c50de3457c82
everttonbs <ebs@ic.ufal.br>
2022-01-26 22:56:25 -0300

    atualização de chave

--- quizzes/settings.py
+++ quizzes/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+from .secret import get_key
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nm*+q&2dscrvu64(ai1ls1_a3@yxqsi2kwei+n1(85*72bstd('
+SECRET_KEY = get_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
